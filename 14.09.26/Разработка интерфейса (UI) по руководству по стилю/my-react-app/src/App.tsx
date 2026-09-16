d9ad8fd6ed07a95bcce03023993b2b6ba4b06627
tonkhao <33198781+tonkhao@users.noreply.github.com>
2026-09-16 15:00:38 +0300

    PartnerCard fix

--- 14.09.26/Разработка интерфейса (UI) по руководству по стилю/my-react-app/src/App.tsx
+++ 14.09.26/Разработка интерфейса (UI) по руководству по стилю/my-react-app/src/App.tsx
@@ -2,16 +2,16 @@ import './App.css'
 import PartnerCard from './components/PartnerCard'
 
 function App() {
-  const partners = [
-    {id: 1, name: "1", discount: 10},
-    {id: 2, name: "2", discount: 20},
-    {id: 3, name: "3", discount: 30}
-  ]
-  return (
-    <>
-        {partners.map(partner => <PartnerCard id = {partner.id} name = {partner.name} discount = {partner.discount}  />)}
-    </>
-  )
+    const partners = [
+        {id: 1, name: "1", discount: 10},
+        {id: 2, name: "2", discount: 20},
+        {id: 3, name: "3", discount: 30}
+    ]
+    return (
+        <>
+            {partners.map(partner => <PartnerCard id={partner.id} name={partner.name} discount={partner.discount}/>)}
+        </>
+    )
 }
 
 export default App
--- 14.09.26/Разработка интерфейса (UI) по руководству по стилю/my-react-app/src/components/PartnerCard.tsx
+++ 14.09.26/Разработка интерфейса (UI) по руководству по стилю/my-react-app/src/components/PartnerCard.tsx
@@ -1,5 +1,17 @@
-function PartnerCard() {
-    return 123
+interface PartnerCardProps {
+    id: number
+    name: string
+    discount: number
+}
+
+function PartnerCard({ id, name, discount }: PartnerCardProps) {
+    return (
+        <div>
+            <p>{id}</p>
+            <p>{name}</p>
+            <p>{discount}%</p>
+        </div>
+    )
 }
 
 
